263aab1dd8a360626918f9e02cc9a488504f9da5
Kongkon Bora <kbora3525@gmail.com>
2025-04-16 11:39:26 +0530

    category

--- Frontend/electron/main.js
+++ Frontend/electron/main.js
@@ -31,6 +31,7 @@ const createWindow = () => {
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '../index.html'));
+    mainWindow.webContents.openDevTools();
   }
 
   // IPC handlers
